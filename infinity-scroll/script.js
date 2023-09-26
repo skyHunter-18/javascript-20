1cf49d9a34448ffcf49b2d1f1a77829201af2945
Tushar Gupta <tushargupta.ws@gmail.com>
2023-09-26 22:05:04 +0530

    added displayPhotos() but rate limit exceeded

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -3,8 +3,29 @@ const loader = document.getElementById("loader");
 
 let photosArray = [];
 
+// Create elements for Links and photos
+function displayPhotos() {
+    // Run foreach for each object in photosArray
+    photosArray.forEach((photo) => {
+        // Creating an <a> element to link to unsplash
+        const item = document.createElement("a");
+        item.setAttribute("href", photo.links.html);
+        item.setAttribute("targer", "_blank");
+
+        // Creating img for photo
+        const img = document.createElement("img");
+        img.setAttribute("src", photo.urls.regular);
+        img.setAttribute("alt", photo.alt_description);
+        img.setAttribute("title", photo.alt_description);
+
+        // Putting img inside a and both inside image-container
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
 // Unsplash API
-const photoCount = 10;
+const photoCount = 2;
 const apiKey = "hFJU40IXy1iaJqblPpxwBZlkf1HZV8SyOsGR2hWKWwM";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${photoCount}`;
 
@@ -13,7 +34,8 @@ async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
         photosArray = await response.json();
-        console.log(photosArray);
+        displayPhotos();
+        // console.log(photosArray);
     } catch (error) {
         // Catch error here
     }
